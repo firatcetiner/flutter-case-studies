b85bb701c7214685aa8b63bd0121920010c28385
firat <f1r4tc@gmail.com>
2020-02-09 23:58:24 +0300

    Fix imports on HomeScreen and ExamplePage

--- animated_search_bar/lib/homescreen/home_screen.dart
+++ animated_search_bar/lib/homescreen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:receipt_scan/homescreen/homescreen_bloc.dart';
-import 'package:receipt_scan/pages/example_page.dart';
+import 'package:animated_search_bar/homescreen/homescreen_bloc.dart';
+import 'package:animated_search_bar/pages/example_page.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
--- animated_search_bar/lib/pages/example_page.dart
+++ animated_search_bar/lib/pages/example_page.dart
@@ -1,6 +1,6 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:receipt_scan/homescreen/homescreen_bloc.dart';
+import 'package:animated_search_bar/homescreen/homescreen_bloc.dart';
 
 class ExamplePage extends StatefulWidget {
 
